41ae95df2852ef13e338bffa58af9087a0699d9c
GryAt0m <117874388+GryAt0m@users.noreply.github.com>
2024-07-29 06:36:25 +0530

    Add files via upload

---
+++ R introduction.R
@@ -0,0 +1,177 @@
+name = "Vishwajeet"
+name2 = "Dipak"
+name2
+age = 25
+age
+NAME3 = "samarth"
+
+y = 20
+
+c = "20"
+NAme = "Kharat"
+NAme
+age = age+19
+c = c+20
+4*8
+c = 5*3
+c= c+20
+
+y
+
+
+
+
+
+
+
+text = "Excellent"
+paste("ExcelR is",text)       #(paste is the fuction) always combine the input
+paste("Student are",text)
+paste("My Name is", name)
+paste("Student are ",text,"in class")
+paste("Vishwajeet are ",text)
+paste("Vishwajeet","Vaishnavi")
+
+
+
+# Assign same value for multiple variables in one line
+var1 = var2 = var3 = "Green"
+var1
+
+
+# legal variable Names
+
+myvar="Vishwajeet"  # all small char
+my_var="Omkar"      # allow undercrous
+maVar="Samarth"     # allow Samall and Capital
+myvar2= "Dipak"     # allow Number
+
+
+# Illegal Variables name
+2myvar = "s"
+my-var = "s"
+_my_var = "s"
+my_v@ar = "s"
+TRUE = "s"
+
+
+
+# Data Type in R
+
+x = 10  # Numeric data
+class(x)
+class(name)
+
+
+# Integer - whole +ve -ve L
+
+y = 100L
+class(y)
+
+
+# Character/String
+b = "R is Exciting"
+class(b)
+
+
+
+# Logical / boolean
+
+ x= TRUE # or T, FALSE,F
+class(x)
+x = F
+class(x)
+
+
+3*5
+3>5
+5>3
+5==5  # COmparing
+
+1 !=6 # Not equal 
+
+# Airthmatic Operator
+
+x = 20
+y = 10
+
+x+y
+x-y
+x*y
+x/y # Float/DEcimal
+
+x %/% y # integer DIvisin
+x^2   # Power
+x %% y # modulus
+
+x
+
+2*p
+2*pi
+pi
+??Constants
+LETTERS
+month.name
+letter
+letters
+month.abb
+
+# Build in  Math Function
+
+max(25,89,34)
+min(28,38,598)
+sqrt(21)
+abs(-4.5)   #Absulute Values
+
+
+#Data Streucture: Vectors,DataFrames
+#Homogeneous Vectors
+
+
+#vector of String same data Store
+
+fruits = c("Banana","Apple","ORange","Stroberry")
+class(fruits)
+
+tools = c("bat","ball","pad")
+class(tools)
+
+n1 = c(1,2,3,4)
+class(n1)
+
+
+n2 = c(T,F ,TRUE ,FALSE)
+class(n2)
+
+
+# Hetrogeneous Vector
+
+mix = c(81,3.5,TRUE,"MAngo",10L)  # LArge is the character hence class is cahracter
+class(mix)
+
+
+x = c(2,3,345,2)
+y = c(34,454,5,4)
+
+x+y
+x*4
+(x+y)*2
+
+
+# Sequence Manner 
+
+1:10
+
+20:30
+999:1000
+
+# start value,end value,step/increment
+seq(1    ,50    ,5)
+    stret, middle value,increment
+    
+seq(10,1,-2)  
+
+seq(10,20,2)
+
+
+
